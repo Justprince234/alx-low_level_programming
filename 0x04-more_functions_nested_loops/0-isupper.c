56775c14aa9e7a893a67cadef776a974362904c4
princewilljackson <princewilljackson@ymail.com>
2022-06-10 15:52:36 -0700

    First commit.

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - function
+ *
+ * @i: parameter
+ *
+ * Return: return value
+ */
+int _isupper(int i)
+{
+	if (i >= 65 && i <= 90)
+		return (1);
+	else
+		return (0);
+}
